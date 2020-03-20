0118d300360cd7ef2bd3f80071ed47a36b5aa368
miasama <jackthecodemonkey@gmail.com>
2020-03-20 20:58:54 +1100

    add more unit tests

--- src/models/AccountsModel.rs
+++ src/models/AccountsModel.rs
@@ -6,7 +6,10 @@ pub struct Accounts {
 }
 
 impl Accounts {
-    pub fn FindByAccountNo<'a>(&'a mut self, accountNo: u32) -> Result<&'a mut Account, &'static str> {
+    pub fn FindByAccountNo<'a>(
+        &'a mut self,
+        accountNo: u32,
+    ) -> Result<&'a mut Account, &'static str> {
         let mut iter = self.accounts.iter_mut();
         return match iter.find(|acc| acc.no == accountNo) {
             Some(Account) => Ok(Account),
@@ -16,5 +19,54 @@ impl Accounts {
 
     pub fn AddAccount(&mut self, account: Account) {
         &self.accounts.push(account);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn should_find_account_by_no() {
+        let account = Account::new(1, String::from("Jack"), 1000);
+        let account2 = Account::new(2, String::from("Seiko"), 5000);
+        let mut accounts = Vec::new();
+        accounts.push(account);
+        accounts.push(account2);
+
+        let mut acc = Accounts { accounts };
+
+        if let Ok(accFound) = acc.FindByAccountNo(2) {
+            assert_eq!(accFound.name, String::from("Seiko"));
+        }
+    }
+
+    #[test]
+    fn should_not_find_accout_by_no() {
+        let account = Account::new(1, String::from("Jack"), 1000);
+        let account2 = Account::new(2, String::from("Seiko"), 5000);
+        let mut accounts = Vec::new();
+        accounts.push(account);
+        accounts.push(account2);
+
+        let mut acc = Accounts { accounts };
+
+        if let Ok(accFound) = acc.FindByAccountNo(4) {
+            assert_eq!(accFound.name, String::from("Seiko"));
+        } else {
+            assert!(true);
+        }
+    }
+
+    #[test]
+    fn can_add_account() {
+        let account = Account::new(1, String::from("Jack"), 1000);
+        let account2 = Account::new(2, String::from("Seiko"), 5000);
+        let mut accounts = Vec::new();
+        accounts.push(account);
+
+        let mut acc = Accounts { accounts };
+        acc.AddAccount(account2);
+
+        assert_eq!(acc.accounts.len(), 2);
     }
 }
--- src/models/BankModel.rs
+++ src/models/BankModel.rs
@@ -45,3 +45,73 @@ impl Bank {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn can_add_account() {
+        let account = Account::new(1, String::from("Jack"), 1000);
+        let account2 = Account::new(2, String::from("Seiko"), 5000);
+        let mut accounts = Vec::new();
+        accounts.push(account);
+
+        let acc = Accounts { accounts };
+        let mut bank = Bank { accounts: acc };
+
+        bank.AddAccount(account2);
+        assert_eq!(bank.accounts.accounts.len(), 2);
+    }
+
+    #[test]
+    fn find_by_no() {
+        let account = Account::new(1, String::from("Jack"), 1000);
+        let mut accounts = Vec::new();
+        accounts.push(account);
+
+        let acc = Accounts { accounts };
+        let mut bank = Bank { accounts: acc };
+        if let Ok(account) = bank.FindByAccountNo(1) {
+            assert_eq!(account.name, String::from("Jack"));
+        }
+    }
+
+    #[test]
+    fn should_deposit() {
+        let account = Account::new(1, String::from("Jack"), 1000);
+        let mut accounts = Vec::new();
+        accounts.push(account);
+
+        let acc = Accounts { accounts };
+        let mut bank = Bank { accounts: acc };
+
+        if let Ok(jack_account) = bank.Deposit(1, 500) {
+            assert_eq!(jack_account.deposit, 1500);
+        }
+    }
+
+    #[test]
+    fn should_transfer() {
+        let account = Account::new(1, String::from("Jack"), 1000);
+        let account2 = Account::new(2, String::from("Seiko"), 5000);
+        let mut accounts = Vec::new();
+        accounts.push(account);
+        accounts.push(account2);
+
+        let acc = Accounts { accounts };
+        let mut bank = Bank { accounts: acc };
+
+        let transfer_from_jack_to_seiko = Transfer {
+            from: 1,
+            to: 2,
+            amount: 750,
+        };
+
+        bank.Transfer(transfer_from_jack_to_seiko);
+
+        if let Ok(seiko_account) = bank.FindByAccountNo(2) {
+            assert_eq!(seiko_account.deposit, 5750);
+        }
+    }
+}
