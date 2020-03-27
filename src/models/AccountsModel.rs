82c45cf1bc05ea4b72f80a5cc2996888b4522043
miasama <jackthecodemonkey@gmail.com>
2020-03-27 20:40:56 +1100

    added more unit tests for delete account method

--- src/models/AccountsModel.rs
+++ src/models/AccountsModel.rs
@@ -30,14 +30,22 @@ impl Accounts {
     }
 
     pub fn DeleteAccount(&mut self, accountNo: u32) -> Result<(), &str> {
-        let len: usize = self.accounts.len();
-        let index = self.accounts.iter().position(|account| account.no == accountNo).unwrap();
-        self.accounts.remove(index);
-        if len != self.accounts.len() {
-            return Err("Failed to delete the account.")
+        if self.HasAccount(accountNo) {
+            let len: usize = self.accounts.len();
+            let index = self
+                .accounts
+                .iter()
+                .position(|account| account.no == accountNo)
+                .unwrap();
+            self.accounts.remove(index);
+            if len == self.accounts.len() {
+                return Err("Failed to delete the account.");
+            }
+            return Ok(());
+        } else {
+            return Err("Account not exists");
         }
-        return Ok(());
-    } 
+    }
 }
 
 #[cfg(test)]
@@ -86,5 +94,32 @@ mod tests {
         acc.AddAccount(account2);
 
         assert_eq!(acc.accounts.len(), 2);
+    }
+
+    #[test]
+    fn should_delete_account() {
+        let account = Account::new(1, String::from("Jack"), 1000);
+        let account2 = Account::new(2, String::from("Seiko"), 5000);
+        let mut accounts = Vec::new();
+        accounts.push(account);
+        accounts.push(account2);
+        let mut acc = Accounts { accounts };
+
+        let _ = acc.DeleteAccount(1).unwrap();
+        assert_eq!(acc.accounts.len(), 1);
+    }
+
+    #[test]
+    fn should_not_delete_account() {
+        let account = Account::new(1, String::from("Jack"), 1000);
+        let account2 = Account::new(2, String::from("Seiko"), 5000);
+        let mut accounts = Vec::new();
+        accounts.push(account);
+        accounts.push(account2);
+        let mut acc = Accounts { accounts };
+
+        if let Err(e) = acc.DeleteAccount(10) {
+            assert_eq!(e, "Account not exists");
+        }
     }
 }
