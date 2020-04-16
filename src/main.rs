2c6b68f20d8558bac424fed13ccad7bcd5b764c2
miasama <jackthecodemonkey@gmail.com>
2020-04-16 21:51:07 +1000

    showing menus

--- src/main.rs
+++ src/main.rs
@@ -55,25 +55,43 @@ impl ValidateCommands for Commands {
 }
 
 fn main() {
-    let arguments: Commands = Commands::new(env::args().skip(1).collect());
+    // Initialize bank service
 
-    let valid_commands = ValidCommands {
-        valid_commands: vec![String::from("use-file"), String::from("use-db")],
-    };
+    // let arguments: Commands = Commands::new(env::args().skip(1).collect());
+    // let valid_commands = ValidCommands {
+    //     valid_commands: vec![String::from("use-file"), String::from("use-db")],
+    // };
 
-    if let Err(e) = arguments.validate(&valid_commands) {
-        println!("{}", e);
-        panic!("Failed to parse command line arguments")
-    }
+    // if let Err(e) = arguments.validate(&valid_commands) {
+    //     println!("{}", e);
+    //     panic!("Failed to parse command line arguments")
+    // }
 
-    let db_context_type = &arguments.arguments[0];
-    let file_type:String = String::from("use-file");
-    let db_type:String = String::from("use-db");
-    let mut bankService = match db_context_type {
-        file_type => BankService::new(FileDBContext {
-            context: FileContext::new("./src/dataSource/data.txt"),
-            transaction_context: FileContext::new("./src/dataSource/transaction.txt"),
-        }),
-    };
-    bankService.Deposit(1, 12300);
+    // let db_context_type = &arguments.arguments[0];
+    // let file_type:String = String::from("use-file");
+    // let db_type:String = String::from("use-db");
+    // let mut bankService = match db_context_type {
+    //     file_type => BankService::new(FileDBContext {
+    //         context: FileContext::new("./src/dataSource/data.txt"),
+    //         transaction_context: FileContext::new("./src/dataSource/transaction.txt"),
+    //     }),
+    // };
+    
+    // Ready to use bankservice 
+
+    // Show menu to user
+    // 1. Add Account
+    // 2. Delete Account
+    // 3. Deposit
+    // 4. Withdraw
+    // 5. Transfer
+    // 6. Exit
+
+    println!("****Choose transaction****");
+    println!("1. Add Account");
+    println!("2. Delete Account");
+    println!("3. Deposit");
+    println!("4. Withdraw");
+    println!("5. Transfer");
+    println!("6. Exit");
 }
