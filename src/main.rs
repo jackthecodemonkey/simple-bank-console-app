c13f98925c39a2fa0f7166cdddbb94ac7b278887
miasama <jackthecodemonkey@gmail.com>
2020-04-23 22:02:04 +1000

    removed codes of validating arguments

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,15 @@
 mod models;
 mod services;
 mod traits;
-use models::Commands::{ Commands, ValidCommands };
-use services::BankService::BankService;
-use std::env;
+use models::Commands::Commands;
 use models::FileContext::FileContext;
-use services::FileDBContext::FileDBContext;
-use traits::ValidateCommands::ValidateCommands;
 use models::View::View;
+use services::BankService::BankService;
+use services::FileDBContext::FileDBContext;
+use std::env;
 
 fn main() {
     let arguments: Commands = Commands::new(env::args().skip(1).collect());
-    let valid_commands = ValidCommands {
-        valid_commands: vec![String::from("use-file"), String::from("use-db")],
-    };
-
-    if let Err(e) = arguments.validate(&valid_commands) {
-        println!("{}", e);
-        panic!("Failed to parse command line arguments")
-    }
-
     let db_context_type = arguments.arguments[0].as_str();
     let bank_service = match db_context_type {
         "use-file" => BankService::new(FileDBContext {
@@ -31,11 +21,11 @@ fn main() {
             context: FileContext::new("./src/dataSource/data.txt"),
             transaction_context: FileContext::new("./src/dataSource/transaction.txt"),
         }),
-        _ => panic!("Not supported db type")
+        _ => panic!("Not supported db type"),
     };
 
     let mut view = View {
-        service: bank_service
+        service: bank_service,
     };
 
     view.Display();
