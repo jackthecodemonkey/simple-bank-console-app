aadddd2a50d63eaed6a74c6ddaded342aab97eee
Jack <jackthecodemonkey@gmail.com>
2020-04-27 21:42:15 +1000

    add transaction schema & start writing dbcontext

--- src/main.rs
+++ src/main.rs
@@ -15,34 +15,39 @@ use models::AccountModel::Account;
 use services::BankService::BankService;
 use services::FileDBContext::FileDBContext;
 use dbConnections::db_connection::establish_connection;
-
+use schema::accounts::dsl::*;
 use diesel::prelude::*;
 
+struct DBContext {
+    context: schema::accounts::table
+}
+
 fn main() {
+    let connection = establish_connection();
+    let results = accounts.load::<Account>(&connection)
+    .expect("Error loading accounts");
 
-    // use schema::accounts::dsl::*;
+    println!("{:?}", results);
 
-    // let connection = establish_connection();
-    // let results = accounts.load::<Account>(&connection)
-    // .expect("Error loading accounts");
 
-    let arguments: Commands = Commands::new(env::args().skip(1).collect());
-    let db_context_type = arguments.arguments[0].as_str();
-    let bank_service = match db_context_type {
-        "use-file" => BankService::new(FileDBContext {
-            context: FileContext::new("./src/dataSource/data.txt"),
-            transaction_context: FileContext::new("./src/dataSource/transaction.txt"),
-        }),
-        "use-db" => BankService::new(FileDBContext {
-            context: FileContext::new("./src/dataSource/data.txt"),
-            transaction_context: FileContext::new("./src/dataSource/transaction.txt"),
-        }),
-        _ => panic!("Not supported db type"),
-    };
 
-    let mut view = View {
-        service: bank_service,
-    };
+    // let arguments: Commands = Commands::new(env::args().skip(1).collect());
+    // let db_context_type = arguments.arguments[0].as_str();
+    // let bank_service = match db_context_type {
+    //     "use-file" => BankService::new(FileDBContext {
+    //         context: FileContext::new("./src/dataSource/data.txt"),
+    //         transaction_context: FileContext::new("./src/dataSource/transaction.txt"),
+    //     }),
+    //     "use-db" => BankService::new(FileDBContext {
+    //         context: FileContext::new("./src/dataSource/data.txt"),
+    //         transaction_context: FileContext::new("./src/dataSource/transaction.txt"),
+    //     }),
+    //     _ => panic!("Not supported db type"),
+    // };
 
-    view.Display();
+    // let mut view = View {
+    //     service: bank_service,
+    // };
+
+    // view.Display();
 }
